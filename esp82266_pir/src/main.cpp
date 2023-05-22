142af5ac5253c8e11714edd841ee383469d5ae06
Daniel Jaramillo <daniel.jaramilloa7@gmail.com>
2023-05-22 16:47:46 -0500

    esp82266_pir: basic use of PIR using interrupts

--- esp82266_pir/src/main.cpp
+++ esp82266_pir/src/main.cpp
@@ -1,22 +1,40 @@
 #include <Arduino.h>
 
-volatile boolean state = false; // Declare state variable as volatile
+#define timeSeconds 10
 
-void IRAM_ATTR timerISR() {
-  state = !state;
-  digitalWrite(LED_BUILTIN, state);
-}
+// Set GPIOs for LED and PIR Motion Sensor
+const int led = LED_BUILTIN;
+const int motionSensor = 14;
+
+// Timer: Auxiliary variables
+unsigned long now;
+unsigned long lastTrigger = 0;
+boolean startTimer = false;
+
+void IRAM_ATTR detectsMovement();
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-  Serial.begin(9600);
+  Serial.begin(115200);
+  pinMode(motionSensor, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(motionSensor), detectsMovement, RISING);
 
-  timer1_enable(TIM_DIV256, TIM_EDGE, TIM_LOOP);
-  timer1_write(312500); // 1 second = 312500 ticks (80 MHz / 256)
-
-  // Attach the ISR (Interrupt Service Routine) to Timer1
-  timer1_attachInterrupt(timerISR);
+  pinMode(led, OUTPUT);
+  digitalWrite(led, HIGH);
 }
 
 void loop() {
+  now = millis();
+  // Turn off the LED after the number of seconds defined in the timeSeconds variable
+  if(startTimer && (now - lastTrigger > (timeSeconds*1000))) {
+    Serial.println("Motion stopped...");
+    digitalWrite(led, HIGH);
+    startTimer = false;
+  }
+}
+
+void IRAM_ATTR detectsMovement() {
+  Serial.println("MOTION DETECTED!!!");
+  digitalWrite(led, LOW);
+  startTimer = true;
+  lastTrigger = millis();
 }
