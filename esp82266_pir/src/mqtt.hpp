16ea1ea8ba61fd150b4cb69df87ce980b0b0a8ba
Daniel Jaramillo <daniel.jaramilloa7@gmail.com>
2023-05-23 15:08:35 -0500

    esp82266_pir: improve MQTT code
    - improve log messages
    - change MQTT publishing topic

--- esp82266_pir/src/mqtt.hpp
+++ esp82266_pir/src/mqtt.hpp
@@ -3,27 +3,27 @@
 const char* MQTT_BROKER_ADRESS = "*********";
 const uint16_t MQTT_PORT = 1883;
 const char* MQTT_CLIENT_NAME = "ESP8266Client";
-const char* MQTT_TOPIC_NAME = "home";
+const char* MQTT_TOPIC_NAME = "pir/balcony";
 
 WiFiClient espClient;
 PubSubClient mqttClient(espClient);
 
 void publishMessage(const char* topic, const char* message) {
-  mqttClient.publish(topic, message);
-  Serial.print("Message published to topic: ");
-  Serial.println(topic);
+  if(mqttClient.publish(topic, message)) {
+    Serial.println("Message published successfully to topic '" + String(topic) + "' with message '" + String(message) + "'");
+  } else {
+    Serial.println("Error in message publishing to topic '" + String(topic) + "'");
+}
 }
 
 void connect() {
   while (!mqttClient.connected()) {
     Serial.println("Connecting to MQTT broker...");
-
     if (mqttClient.connect(MQTT_CLIENT_NAME)) {
       Serial.println("Connected to MQTT broker");
       mqttClient.subscribe(MQTT_TOPIC_NAME);
     } else {
-      Serial.print("Failed with state ");
-      Serial.print(mqttClient.state());
+      Serial.println("Failed with state " + String(mqttClient.state()) + ", trying again in 2 seconds");
       delay(2000);
     }
   }
